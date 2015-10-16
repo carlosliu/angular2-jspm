cff800a78fabaf052509ac79e1e100e8a92ffa88
Carlos Liu <carlosliu@live.com>
2015-10-16 14:22:19 +1300

    Add TypeScript config and definition (only useful for VSCode)

---
+++ typings/tsd.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="../jspm_packages/npm/angular2@2.0.0-alpha.42/angular2.d.ts" />
+/// <reference path="../jspm_packages/npm/angular2@2.0.0-alpha.42/http.d.ts" />
+/// <reference path="../jspm_packages/npm/angular2@2.0.0-alpha.42/router.d.ts" />
