cb41e10ba560083052690237f3e37119f63b863c
Carlos Liu <carlosliu@live.com>
2015-10-18 17:17:41 +1300

    more update to alpha.44

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,3 @@
-/// <reference path="../jspm_packages/npm/angular2@2.0.0-alpha.42/angular2.d.ts" />
-/// <reference path="../jspm_packages/npm/angular2@2.0.0-alpha.42/http.d.ts" />
-/// <reference path="../jspm_packages/npm/angular2@2.0.0-alpha.42/router.d.ts" />
+/// <reference path="../jspm_packages/npm/angular2@2.0.0-alpha.44/angular2.d.ts" />
+/// <reference path="../jspm_packages/npm/angular2@2.0.0-alpha.44/http.d.ts" />
+/// <reference path="../jspm_packages/npm/angular2@2.0.0-alpha.44/router.d.ts" />
